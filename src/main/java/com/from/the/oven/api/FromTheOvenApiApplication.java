ca665445501f703019c1f8aa6696ce77e064105d
vvasylykha <vvasylykha@unitedthinkers.com>
2023-03-08 22:06:14 +0200

    init priject

---
+++ src/main/java/com/from/the/oven/api/FromTheOvenApiApplication.java
@@ -0,0 +1,13 @@
+package com.from.the.oven.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromTheOvenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromTheOvenApiApplication.class, args);
+	}
+
+}
