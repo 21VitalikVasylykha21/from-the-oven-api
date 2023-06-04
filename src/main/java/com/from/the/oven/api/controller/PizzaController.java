3479bdbd9f53d7e1a04531c45482c57f542d5e0a
vvasylykha <vvasylykha@unitedthinkers.com>
2023-06-04 16:49:30 +0300

    Remove point update\create\delete pizza

--- src/main/java/com/from/the/oven/api/controller/PizzaController.java
+++ src/main/java/com/from/the/oven/api/controller/PizzaController.java
@@ -3,19 +3,14 @@ package com.from.the.oven.api.controller;
 import com.from.the.oven.api.dto.ApiResponse;
 import com.from.the.oven.api.dto.PizzaDTO;
 import com.from.the.oven.api.dto.PizzaSearchResponse;
-import com.from.the.oven.api.entity.Pizza;
+import com.from.the.oven.api.exception.EntityNotFoundException;
 import com.from.the.oven.api.service.PizzaService;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,11 +38,11 @@ public class PizzaController {
 
 	@GetMapping("/{id}")
 	public ApiResponse<PizzaDTO> findPizzaById(@PathVariable Long id) {
-		Optional<Pizza> pizza = pizzaService.findById(id);
-		if (pizza.isEmpty()) {
-			return new ApiResponse<>(HttpStatus.NOT_FOUND);
+		try {
+			return new ApiResponse<>(List.of(new PizzaDTO(pizzaService.findById(id))));
+		} catch (EntityNotFoundException exception) {
+			return new ApiResponse<>(HttpStatus.BAD_REQUEST, exception);
 		}
-		return new ApiResponse<>(List.of(new PizzaDTO(pizza.get())));
 	}
 
 	@GetMapping("/search")
@@ -61,26 +56,5 @@ public class PizzaController {
 				pizzaSearchResponse.pizzas().stream()
 						.map(PizzaDTO::new)
 						.toList());
-	}
-
-	@PatchMapping("/{id}")
-	public ApiResponse<PizzaDTO> updatePizza(@PathVariable Long id,
-											 @RequestBody PizzaDTO updatedPizza) {
-		Optional<Pizza> pizza = pizzaService.update(id, updatedPizza);
-		if (pizza.isEmpty()) {
-			return new ApiResponse<>(HttpStatus.NOT_FOUND);
-		}
-		return new ApiResponse<>(List.of(new PizzaDTO(pizza.get())));
-	}
-
-	@PostMapping("/")
-	public ApiResponse<PizzaDTO> updatePizza(@RequestBody PizzaDTO updatedPizza) {
-		return new ApiResponse<>(List.of(new PizzaDTO(pizzaService.create(updatedPizza))));
-	}
-
-	@DeleteMapping("/{id}")
-	public ApiResponse<PizzaDTO> deletePizza(@PathVariable Long id) {
-		pizzaService.delete(id);
-		return new ApiResponse<>(HttpStatus.OK);
 	}
 }
--- src/main/java/com/from/the/oven/api/service/PizzaService.java
+++ src/main/java/com/from/the/oven/api/service/PizzaService.java
@@ -1,25 +1,15 @@
 package com.from.the.oven.api.service;
 
-import com.from.the.oven.api.dto.PizzaDTO;
 import com.from.the.oven.api.dto.PizzaSearchResponse;
-import com.from.the.oven.api.entity.Category;
-import com.from.the.oven.api.entity.Ingredient;
 import com.from.the.oven.api.entity.Pizza;
-import com.from.the.oven.api.entity.PizzaCategory;
-import com.from.the.oven.api.entity.PizzaCategoryId;
-import com.from.the.oven.api.entity.PizzaIngredient;
-import com.from.the.oven.api.entity.PizzaIngredientId;
-import com.from.the.oven.api.entity.PizzaPrice;
-import com.from.the.oven.api.enums.PizzaSize;
+import com.from.the.oven.api.exception.EntityNotFoundException;
 import com.from.the.oven.api.repository.CategoryRepository;
 import com.from.the.oven.api.repository.IngredientRepository;
 import com.from.the.oven.api.repository.PizzaCategoryRepository;
 import com.from.the.oven.api.repository.PizzaIngredientRepository;
 import com.from.the.oven.api.repository.PizzaPriceRepository;
 import com.from.the.oven.api.repository.PizzaRepository;
-import io.micrometer.common.util.StringUtils;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -54,8 +44,12 @@ public class PizzaService {
 		return pizzaRepository.findAll(PageRequest.of(0, limit)).getContent();
 	}
 
-	public Optional<Pizza> findById(Long id) {
-		return pizzaRepository.findById(id);
+	public Pizza findById(Long id) {
+		Optional<Pizza> pizza = pizzaRepository.findById(id);
+		if (pizza.isEmpty()) {
+			throw new EntityNotFoundException(Pizza.class, id);
+		}
+		return pizza.get();
 	}
 
 	public PizzaSearchResponse search(String name, List<String> ingredients, List<String> categories, Integer limit, Integer page) {
@@ -65,127 +59,5 @@ public class PizzaService {
 
 	public PizzaSearchResponse searchByName(String name) {
 		return search(name, List.of(""), List.of(""), 20, 1);
-	}
-
-	public Optional<Pizza> update(Long id, PizzaDTO updatePizzaDto) {
-		Optional<Pizza> optionalPizza = findById(id);
-		if (optionalPizza.isEmpty()) {
-			return Optional.empty();
-		}
-		Pizza existingPizza = optionalPizza.get();
-		updatePriceTable(existingPizza, updatePizzaDto);
-		updatePizzaInfo(existingPizza, updatePizzaDto);
-		updatePizzaCategory(existingPizza, updatePizzaDto);
-		updatePizzaIngredient(existingPizza, updatePizzaDto);
-		return findById(id);
-	}
-
-	public Pizza create(PizzaDTO updatePizzaDto) {
-		return new Pizza();
-	}
-
-	public void delete(Long id) {
-		Optional<Pizza> optionalPizza = findById(id);
-		if (optionalPizza.isEmpty()) {
-			throw new IllegalArgumentException("Not found pizza");
-		}
-		pizzaRepository.delete(optionalPizza.get());
-	}
-
-	private void updatePizzaInfo(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		existingPizza.setName(StringUtils.isNotBlank(updatePizzaDto.getName()) ? updatePizzaDto.getName() : existingPizza.getName());
-		existingPizza.setDescription(StringUtils.isNotBlank(updatePizzaDto.getDescription()) ? updatePizzaDto.getDescription() : existingPizza.getDescription());
-		existingPizza.setImage(StringUtils.isNotBlank(updatePizzaDto.getImage()) ? updatePizzaDto.getImage() : existingPizza.getImage());
-		existingPizza.setRating(Objects.isNull(updatePizzaDto.getRating()) ? existingPizza.getRating() : updatePizzaDto.getRating());
-		pizzaRepository.save(existingPizza);
-	}
-
-	private void updatePizzaIngredient(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		addIngredient(existingPizza, updatePizzaDto);
-		removeIngredient(existingPizza, updatePizzaDto);
-	}
-
-	private void updatePizzaCategory(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		addCategory(existingPizza, updatePizzaDto);
-		removeCategory(existingPizza, updatePizzaDto);
-	}
-
-	private void updatePriceTable(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		PizzaPrice smallPizzaPrice = existingPizza.getPrices().stream()
-				.filter(price -> price.getSize().getName().equals(PizzaSize.SMALL))
-				.findFirst()
-				.get();
-		if (updatePizzaDto.getPriceSmall() != null && !smallPizzaPrice.getPrice().equals(updatePizzaDto.getPriceSmall())) {
-			smallPizzaPrice.setPrice(updatePizzaDto.getPriceSmall());
-			pizzaPriceRepository.save(smallPizzaPrice);
-		}
-		if (updatePizzaDto.getMassSmall() != null && !smallPizzaPrice.getMass().equals(updatePizzaDto.getMassSmall())) {
-			smallPizzaPrice.setMass(updatePizzaDto.getMassSmall());
-			pizzaPriceRepository.save(smallPizzaPrice);
-		}
-		PizzaPrice largePizzaPrice = existingPizza.getPrices().stream()
-				.filter(price -> price.getSize().getName().equals(PizzaSize.LARGE))
-				.findFirst()
-				.get();
-		if (updatePizzaDto.getPriceLarge() != null && !largePizzaPrice.getPrice().equals(updatePizzaDto.getPriceLarge())) {
-			largePizzaPrice.setPrice(updatePizzaDto.getPriceLarge());
-			pizzaPriceRepository.save(largePizzaPrice);
-		}
-		if (updatePizzaDto.getMassLarge() != null && !largePizzaPrice.getMass().equals(updatePizzaDto.getMassLarge())) {
-			largePizzaPrice.setMass(updatePizzaDto.getMassLarge());
-			pizzaPriceRepository.save(largePizzaPrice);
-		}
-	}
-
-	private void addIngredient(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		for (String ingredientName : updatePizzaDto.getIngredients()) {
-			if (existingPizza.getIngredients().contains(new Ingredient(ingredientName))) {
-				continue;
-			}
-			Optional<Ingredient> optionalIngredient = ingredientRepository.findByName(ingredientName);
-			if (optionalIngredient.isEmpty()) {
-				throw new IllegalArgumentException("Not found ingredient: " + ingredientName);
-			}
-			Ingredient ingredient = optionalIngredient.get();
-			pizzaIngredientRepository.save(new PizzaIngredient(new PizzaIngredientId(existingPizza.getId(), ingredient.getId())));
-			existingPizza.getIngredients().add(ingredient);
-		}
-	}
-
-	private void removeIngredient(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		for (int i = 0; i < existingPizza.getIngredients().size(); i++) {
-			Ingredient ingredient = existingPizza.getIngredients().get(i);
-			if (updatePizzaDto.getIngredients().contains(ingredient.getName())) {
-				continue;
-			}
-			pizzaIngredientRepository.delete(new PizzaIngredient(new PizzaIngredientId(existingPizza.getId(), ingredient.getId())));
-			existingPizza.getIngredients().remove(ingredient);
-		}
-	}
-
-	private void addCategory(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		for (String categoryName : updatePizzaDto.getCategories()) {
-			if (existingPizza.getCategories().contains(new Category(categoryName))) {
-				continue;
-			}
-			Optional<Category> optionalCategory = categoryRepository.findByName(categoryName);
-			if (optionalCategory.isEmpty()) {
-				throw new IllegalArgumentException("Not found category: " + categoryName);
-			}
-			Category category = optionalCategory.get();
-			pizzaCategoryRepository.save(new PizzaCategory(new PizzaCategoryId(existingPizza.getId(), category.getId())));
-			existingPizza.getCategories().add(category);
-		}
-	}
-
-	private void removeCategory(Pizza existingPizza, PizzaDTO updatePizzaDto) {
-		for (int i = 0; i < existingPizza.getCategories().size(); i++) {
-			Category category = existingPizza.getCategories().get(i);
-			if (updatePizzaDto.getCategories().contains(category.getName())) {
-				continue;
-			}
-			pizzaCategoryRepository.delete(new PizzaCategory(new PizzaCategoryId(existingPizza.getId(), category.getId())));
-			existingPizza.getCategories().remove(category);
-		}
 	}
 }
